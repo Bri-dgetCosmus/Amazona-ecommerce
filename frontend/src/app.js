9a4573f883380232a2cf61bd22c5bb499b320000
sebbyapudo <sebbyapudo@gmail.com>
2023-06-16 02:46:27 +0300

    Updated the router function and implemented the different routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,22 @@
+import Error404Screen from './screens/Error404Screen.js';
 import HomeScreen from './screens/HomeScreen.js'
-
+import ProductScreen from './screens/ProductSreen.js';
+import { parseRequestUrl } from './utils.js';
+// routes
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
+}
 // router funuction
 const router = () => {
-  const main = document.getElementById('main-container');
-  main.innerHTML = HomeScreen.render()
-};
+  const request = parseRequestUrl();
+  const parseUrl = 
+    (request.resource ? `/${request.resource}` : '/') +  (request.id ? '/:id' : '') + 
+    (request.verb ? `/${request.verb}` : '');
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
 
+  const main = document.getElementById('main-container');
+  main.innerHTML = screen.render();
+};
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
